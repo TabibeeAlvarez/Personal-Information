773d7492ed8606ed3e051ef4211794bdd81d6126
eXclusive Tshirt <116873249+TabibeeAlvarez@users.noreply.github.com>
2024-02-08 01:22:50 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+document.getElementById('pop-bg').style.display = 'none'
+
+function pasok(){
+    const username = document.getElementById('user').value;
+    const password = document.getElementById('pass').value;
+
+    if(username === 'tabibee' && password === '12345'){
+        window.location.href = '/inside/index-inside.html'
+    }
+    else{
+        document.getElementById('pop-bg').style.display = 'block'
+        document.getElementById('bg').style.display = 'none'
+    }
+    event.preventDefault();
+}
+
+function back1(){
+    window.location.href = '/index.html'
+}
+
+const popUp = document.querySelectorAll('.pop-paper');
+window.addEventListener('click', pops);
+
+function pops(){
+    const triggerBottom = window.innerHeight / 3 * 2;
+
+    popUp.forEach(puppy => {
+        const puppyTop = puppy.getBoundingClientRect().top;
+
+        if(puppyTop < triggerBottom){
+            puppy.classList.add('popUp');
+        }
+        else{
+            puppy.classList.remove('popUp');
+        }
+    })
+}
+
